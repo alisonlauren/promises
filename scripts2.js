ee968aef346eeb472e2d424d9a00e10e97a6ab7e
Alison Manning <alisonlaurenmanning@gmail.com>
2020-09-17 12:02:07 -0500

    add

--- scripts2.js
+++ scripts2.js
@@ -1,21 +1,34 @@
+function fetchUrl(url) {
+return fetch(url)
+    .then(res => res.json())
+}
 fetch('https://anapioficeandfire.com/api/books/1')
-    .then((res) => {
-        return res.json()
+.then((res) => {
+    return res.json()
+})
+.then((bookData) => {
+    console.log(bookData)
+    // add title
+    const title = document.getElementById('title');
+    title.textContent = bookData.name;
+    // add authors
+    const ul = document.getElementById('authors');
+    bookData.authors.forEach((author) => {
+    const authorLi = document.createElement('li');
+    authorLi.textContent = author;
+    ul.appendChild(authorLi);
     })
-    .then(bookData => {
-        console.log(bookData)
-        //add title
-        const title = document.createElement('h1');
-        title.textContent = bookData.name
-        document.body.appendChild(title);
-
-        //add authors to the page
-        const ul = document.createElement('ul');
-        document.body.appendChild(ul);
-
-        bookData.authors.forEach(author => {
-            const authorLi = document.createElement('li');
-            authorLi.textContent = author;
-            ul.appendChild(authorLi);
+    const charPromises = bookData.povCharacters.map((url) =>{
+    return fetchUrl(url)
+    })
+    Promise.all(charPromises)
+    .then(characters => {
+        console.log(characters);
+        const charList = document.getElementById('characters');
+        characters.forEach(char => {
+        const li = document.createElement('li');
+        li.textContent = char.name;
+        charList.appendChild(li)
         })
     })
+})
